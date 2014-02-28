02b2d603dab625e6c1c1e14d518ca0fd5a83cecb
Rodolfo Campos <camposer@gmail.com>
2014-02-28 09:00:10 +0100

    eMarketWeb con EMF, EM, TransactionManager y manejo de transacciones por Spring en archivo de configuraciones

--- eMarketWeb/src/es/indra/formacion/pr/spring/config/Config.java
+++ eMarketWeb/src/es/indra/formacion/pr/spring/config/Config.java
@@ -1,11 +1,17 @@
 package es.indra.formacion.pr.spring.config;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import es.indra.formacion.pr.spring.dao.IProductoDao;
 import es.indra.formacion.pr.spring.dao.ProductoDaoJpa;
-import es.indra.formacion.pr.spring.dao.ProductoDaoMockup;
 import es.indra.formacion.pr.spring.service.IProductoService;
 import es.indra.formacion.pr.spring.service.ProductoService;
 
@@ -13,6 +19,8 @@ import es.indra.formacion.pr.spring.service.ProductoService;
  * La clase debe tener la anotación \@Configuration 
  */
 @Configuration
+// <tx:annotation-driven transaction-manager="transactionManager"/-->
+@EnableTransactionManagement
 public class Config {
 	/**
 	 * El nombre del método es el id del Bean 
@@ -27,4 +35,19 @@ public class Config {
 		return new ProductoService();
 	}
 	
+	@Bean
+	public EntityManagerFactory entityManagerFactory() {
+		return Persistence.createEntityManagerFactory("eMarketWeb");
+	}
+	
+	@Bean 
+	public EntityManager entityManager() {
+		return entityManagerFactory()
+				.createEntityManager();
+	}
+	
+	@Bean 
+	public PlatformTransactionManager transactionManager() {
+		return new JpaTransactionManager(entityManagerFactory());
+	}
 }
